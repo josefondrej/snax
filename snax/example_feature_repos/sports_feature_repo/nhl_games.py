fa333aa9905c3deea388474b0d58b3f41c47b97d
Josef Ondrej <josef.ondrej@outlook.com>
2022-07-19 15:43:57 +0200

    Add example feature repos

---
+++ snax/example_feature_repos/sports_feature_repo/nhl_games.py
@@ -0,0 +1,36 @@
+from pathlib import Path
+
+from snax.csv_data_source import CsvDataSource
+from snax.feature import Feature
+from snax.feature_view import FeatureView
+from snax.value_type import Int, String, Timestamp, Float
+
+data_path = Path(__file__).parent.parent / 'data' / 'game.csv'
+
+data_source = CsvDataSource(
+    name='nhl_games_csv',
+    csv_file_path=data_path
+)
+
+FeatureView(
+    name='nhl_games_csv',
+    entities=None,
+    features=[
+        Feature('game_id', Int),
+        Feature('season', String),
+        Feature('type', String),
+        Feature('date_time_GMT', Timestamp),
+        Feature('away_team_id', Int),
+        Feature('home_team_id', Int),
+        Feature('away_goals', Int),
+        Feature('home_goals', Int),
+        Feature('outcome', String),
+        Feature('home_rink_side_start', String),
+        Feature('venue', String),
+        Feature('venue_link', String),
+        Feature('venue_time_zone_id', String),
+        Feature('venue_time_zone_offset', Float),
+        Feature('venue_time_zone_tz', String),
+    ],
+    data_source=data_source
+)
---
+++ snax/example_feature_repos/winequality_feature_repo/winequality.py
@@ -0,0 +1,31 @@
+from pathlib import Path
+
+from snax.csv_data_source import CsvDataSource
+from snax.feature import Feature
+from snax.feature_view import FeatureView
+from snax.value_type import Float
+
+data_path = Path(__file__).parent.parent / 'data' / 'winequality.csv'
+
+data_source = CsvDataSource(
+    name='winequality_csv',
+    csv_file_path=data_path,
+    separator=',',
+    field_mapping={
+        'citric_acid': 'citric_acidity',
+        'pH': 'ph'
+    },
+    tags={'owner': 'john.doe@gmail.com'}
+)
+
+FeatureView(
+    name='winequality_acidity_csv',
+    entities=None,
+    features=[
+        Feature('volatile_acidity', Float),
+        Feature('fixed_acidity', Float),
+        Feature('citric_acidity', Float),
+        Feature('ph', Float)
+    ],
+    data_source=data_source
+)
