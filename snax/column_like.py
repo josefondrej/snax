ca8195a89fe3db3d842e5ed84772c4e07a18b9bc
Josef Ondrej <josef.ondrej@outlook.com>
2022-07-20 11:29:21 +0200

    Add column_like.get_colnames

--- snax/column_like.py
+++ snax/column_like.py
@@ -1,6 +1,17 @@
-from typing import Union
+from typing import Union, List
 
 from snax.entity import Entity
 from snax.feature import Feature
 
 ColumnLike = Union[Entity, Feature, str]
+
+
+def get_colnames(column_like: ColumnLike) -> List[str]:
+    if isinstance(column_like, Entity):
+        return column_like.join_keys
+    elif isinstance(column_like, Feature):
+        return [column_like.name]
+    elif isinstance(column_like, str):
+        return [column_like]
+    else:
+        raise TypeError(f'Unsupported type {type(column_like)}')
