3824d117b149a5309b9300ae4fb2cfb5d8c220ed
Josef Ondrej <josef.ondrej@outlook.com>
2022-07-22 10:40:05 +0200

    Add more tests to test_csv_data_source.py

--- tests/test_csv_data_source.py
+++ tests/test_csv_data_source.py
@@ -1,8 +1,11 @@
+import pandas as pd
 import pytest
 
 from snax.csv_data_source import CsvDataSource
 from snax.entity import Entity
-from snax.example_feature_repos.sports_feature_repo.nhl_games import data_path as original_data_path
+from snax.example_feature_repos.sports_feature_repo.nhl_games import data_path as original_nhl_data_path
+from snax.example_feature_repos.users_with_nas_feature_repo.users_with_nas import \
+    data_path as original_users_with_na_data_path
 from snax.feature import Feature
 from snax.utils import copy_to_temp
 from snax.value_type import Int
@@ -10,10 +13,22 @@ from snax.value_type import Int
 
 @pytest.fixture
 def nhl_data_source():
-    data_path = copy_to_temp(original_data_path)
+    data_path = copy_to_temp(original_nhl_data_path)
 
     data_source = CsvDataSource(
         name='nhl_games_csv',
+        csv_file_path=data_path
+    )
+
+    return data_source
+
+
+@pytest.fixture
+def users_with_nas_data_source():
+    data_path = copy_to_temp(original_users_with_na_data_path)
+
+    data_source = CsvDataSource(
+        name='users_with_nas_csv',
         csv_file_path=data_path
     )
 
@@ -52,3 +67,23 @@ def test_select_multiple_columns(nhl_data_source):
     )
     expected_data_columns = ['game_id', 'game_id', 'game_id', 'away_team_id']
     assert list(data.columns) == expected_data_columns
+
+
+def test_select_string_column_with_missings(users_with_nas_data_source):
+    data = users_with_nas_data_source.select(['first_name'])
+    assert isinstance(data, pd.DataFrame)
+
+
+def test_select_int_column_with_missings(users_with_nas_data_source):
+    data = users_with_nas_data_source.select(['children'])
+    assert isinstance(data, pd.DataFrame)
+
+
+def test_select_float_column_with_missings(users_with_nas_data_source):
+    data = users_with_nas_data_source.select(['age'])
+    assert isinstance(data, pd.DataFrame)
+
+
+def test_select_timestamp_column_with_missings(users_with_nas_data_source):
+    data = users_with_nas_data_source.select(['timestamp'])
+    assert isinstance(data, pd.DataFrame)
