4bebe2767660891e58f69ad63160df141518ec07
Josef Ondrej <josef.ondrej@outlook.com>
2022-07-26 17:50:06 +0200

    Add csv data source examples

---
+++ snax/data_sources/examples/csv.py
@@ -0,0 +1,30 @@
+from snax.data_sources.csv_data_source import CsvDataSource
+from snax.example_feature_repos.sports_feature_repo.nhl_games import data_path as original_nhl_data_path
+from snax.example_feature_repos.users_with_nas_feature_repo.users_with_nas import \
+    data_path as original_users_with_na_data_path
+from snax.utils import copy_to_temp
+
+
+def create_nhl_games() -> CsvDataSource:
+    return CsvDataSource(
+        name='nhl_games_csv',
+        csv_file_path=copy_to_temp(original_nhl_data_path)
+    )
+
+
+def create_users_with_nas() -> CsvDataSource:
+    return CsvDataSource(
+        name='users_with_nas_csv',
+        csv_file_path=copy_to_temp(original_users_with_na_data_path)
+    )
+
+
+def create_users_with_nas_field_mapping() -> CsvDataSource:
+    return CsvDataSource(
+        name='users_with_nas_csv',
+        csv_file_path=copy_to_temp(original_users_with_na_data_path),
+        field_mapping={
+            'is_subscribed': 'issubscribed',
+            'timestamp': 'time_stamp',
+        }
+    )
